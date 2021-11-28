de04876982304d12bd12090d3db23462f01e71f5
git-a-z <77612399+git-a-z@users.noreply.github.com>
2021-11-28 18:36:19 +0300

    Update sql-template.sql

--- 05/sql-template.sql
+++ 05/sql-template.sql
@@ -1,16 +1,19 @@
-DROP DATABASE IF EXISTS gb_php_az;
-CREATE DATABASE gb_php_az;
-USE gb_php_az;
+/*DROP DATABASE IF EXISTS gb_php2_az_sf;
+CREATE DATABASE gb_php2_az_sf;
+USE gb_php2_az_sf;
 
-DROP TABLE IF EXISTS gb_php_az.image_gallery;
-CREATE TABLE gb_php_az.image_gallery (
+DROP TABLE IF EXISTS gb_php2_az_sf.product;
+CREATE TABLE gb_php2_az_sf.product (
 	id SERIAL PRIMARY KEY,
-	imagePath VARCHAR(255) NOT NULL,
+	title VARCHAR(255) NOT NULL, 
+	description LONGTEXT DEFAULT NULL, 
+	price DOUBLE PRECISION NOT NULL, 
+	image_path VARCHAR(255) NOT NULL, 
 	clicks INT UNSIGNED DEFAULT 0
-) COMMENT 'Галерея картинок';
+) COMMENT 'Галерея картинок';*/
 
-INSERT INTO gb_php_az.image_gallery (imagePath) VALUES
-	('img/Herald.jpg'),
-	('img/HumptyDumpty.jpg'),
-	('img/MadTeaParty.jpg'),
-	('img/WhiteKnight.jpg');
+INSERT INTO gb_php2_az_sf.product (title,price,image_path) VALUES
+	('Herald',111.11,'Herald.jpg'),
+	('HumptyDumpty',222.22,'HumptyDumpty.jpg'),
+	('MadTeaParty',333.33,'MadTeaParty.jpg'),
+	('WhiteKnight',444.44,'WhiteKnight.jpg');
